8a09795e80ab44325d9ab04ca40d426cdd7b4e1c
Chirag Malpani <56476770+chirs21@users.noreply.github.com>
2020-06-09 22:04:04 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,63 @@
+    <div class="footer">
+      <div class="row">
+        <div class="col-sm-2">
+            <div style="background-color:white">Logo</div>
+            <br>
+            <p><a href="aboutus.php">About Us</a></p>
+            <p><a href="#">Partners</a></p>
+            <p><a href="#">Clientele</a></p>
+            <p><a href="#">Testimonials</a></p>
+        </div>
+        <div class="col-sm-2">
+            <p class="header">Browse Opportunities</p>
+            <br>
+            <p><a href="#">Competitions </a>|
+               <a href="#"> Quizzes</a></p>
+            <p><a href="#">Hackathons </a>|
+               <a href="#"> Jobs and Internships</a></p>
+            <p><a href="#">Scholarships </a>|
+               <a href="#"> Workshops</a></p>
+            <p><a href="#">Cultural Events</a>
+        </div>
+        <div class="col-sm-2">
+            <p class="header">Categories<p>
+            <p><a href="#">Industries </a></p>
+            <p><a href="#">Industries-1</a></p>
+            <p><a href="#">Industries-2</a></p>
+            <p><a href="#">Industries-3</a></p>
+            <p><a href="#">Industries-4</a></p>
+            <p><a href="#">Industries-5</a></p>
+            <p><a href="#">Industries-6</a></p>
+        </div>
+        <div class="col-sm-2">
+            <p class="header">Locations</p>
+            <p><a href="#">Delhi </a></p>
+            <p><a href="#">Mumbai</a></p>
+            <p><a href="#">Kolkata</a></p>
+            <p><a href="#">Pune</a></p>
+            <p><a href="#">Bengaluru</a></p>
+            <p><a href="#">Chennai</a></p>
+            <p><a href="#">Ahemdabad</a></p>
+        </div>
+        <div class="col-sm-4">
+            <p class="header">Connect with us</p>
+            <p><a href="#"><i class='fab fa-instagram' style='font-size:24px;color:white'></i>&nbspInstagram </a></p>
+            <p><a href="#"><i class='fab fa-facebook' style='font-size:24px;color:white'></i>&nbspFacebook </a></p>
+            <p><a href="#"><i class='fab fa-whatsapp' style='font-size:24px;color:white'></i>&nbspWhatsapp </a></p>
+            <p><a href="#"><i class='fab fa-linkedin' style='font-size:24px;color:white'></i>&nbspLinkedIn</a></p>
+        </div>
+      </div>
+<hr style="border-top: 1px solid white;">
+       <div class="conditions">
+           <p><a href="terms.php">Terms & Conditions </a>|
+              <a href="#"> Privacy Policy </a>|
+              <a href="#"> Branding Guidelines </a>|
+              <a href="#"> Partner With Us </a>|
+              <a href="#"> Contact Us </a>|
+              <a href="#"> FAQs </a>|
+              <a href="#"> Subscribe Newsletter</a>
+              <a type="button" href="#" class="btn btn-primary" style="color:white">Publish Oppotunity</a>
+           </p>
+        </div> 
+<hr style="border-top: 1px solid white;"><br><br>
+</div> 
