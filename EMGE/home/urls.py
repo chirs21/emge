59bc5b4279aa956fa84d4eec2f64e3dd5d144250
Prachi Singh <47474195+Prach1810@users.noreply.github.com>
2020-06-10 16:59:13 +0530

    Update urls.py

--- EMGE/home/urls.py
+++ EMGE/home/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from home.views import home
+from home.views import home,about,partner,companyprofile,terms,dashboard, menapparel
 
 urlpatterns = [
-    path('home', home, name="home"),
+    path('', home, name="home"),
+    path('about', about, name="about"),
+    path('partner', partner, name="partner"),
+    path('terms', terms, name="terms"),
+    path('dashboard',dashboard , name="dashboard"),
+    path('companyprofile', companyprofile, name="companyprofile"),
+    path('menapparel', menapparel, name="menapparel"),
 ]
