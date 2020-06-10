eb7700eaf9719b8dc1f80d7544e7bcd23b2952ca
Prachi Singh <47474195+Prach1810@users.noreply.github.com>
2020-06-10 16:58:17 +0530

    Update models.py

--- EMGE/home/models.py
+++ EMGE/home/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 # Create your models here.
+class MenApparel(models.Model):
+    product_image = models.ImageField()
+    product_name = models.TextField()
+    product_price = models.CharField(max_length=200, null=True)
+    product_offer = models.FloatField(null=True)
+
+    def __str__(self):
+        return self.product_name
+
+    class Meta:
+        verbose_name_plural = 'menapparels'
+
+
+class womenApparel(models.Model):
+    product_image = models.ImageField()
+    product_name = models.TextField()
+    product_price = models.CharField(max_length=200, null=True)
+    product_offer = models.FloatField(null=True)
+
+    def __str__(self):
+        return self.product_name
+
+    class Meta:
+        verbose_name_plural = 'womenapparels'
+
+class KidApparel(models.Model):
+    product_image = models.ImageField()
+    product_name = models.TextField()
+    product_price = models.CharField(max_length=200, null=True)
+    product_offer = models.FloatField(null=True)
+
+    def __str__(self):
+        return self.product_name
+
+    class Meta:
+        verbose_name_plural = 'kidapparels'
